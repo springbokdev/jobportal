86fc032bacf97c69c4e848f83940fb7556f4ac2a
johnspangenberg <dev@springbok.space>
2024-06-29 10:47:21 +0200

    Initial commit

---
+++ src/test/java/space/springbok/jobportal/JobportalApplicationTests.java
@@ -0,0 +1,13 @@
+package space.springbok.jobportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobportalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
