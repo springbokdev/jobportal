4f51a30bbe6439c65d93a8b204981680db03825c
johnspangenberg <dev@springbok.space>
2024-07-04 16:24:11 +0200

    Finished security.

--- src/main/java/space/springbok/jobportal/controller/UserController.java
+++ src/main/java/space/springbok/jobportal/controller/UserController.java
@@ -1,11 +1,15 @@
 package space.springbok.jobportal.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import space.springbok.jobportal.entity.User;
@@ -48,6 +52,21 @@ public class UserController {
 
         userService.addNew(user);
         return "dashboard";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null) {} {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+        return "redirect:/";
     }
 
 }
